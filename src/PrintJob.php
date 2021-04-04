fc58b44b4b1e1a668d14e20c7ca3c1a0b404e45f
Shane Mc Cormack <shanemcc@gmail.com>
2021-04-04 09:36:12 +0100

    Initial code.

---
+++ src/PrintJob.php
@@ -0,0 +1,189 @@
+<?php
+
+	namespace ShaneMcC\PTouchPrint;
+
+	use Exception;
+
+	class PrintJob {
+		private PTouchJobBinary $job;
+
+		private int $autoCut;
+		private bool $halfCut;
+		private bool $chainPrinting;
+		private int $tapeSize;
+		private int $marginSize;
+
+		private bool $hasImages = false;
+		private int $jobState = PrintJob::JOBSTATE_NONE;
+
+		public const JOBSTATE_NONE = 0;
+		public const JOBSTATE_START = 1;
+		public const JOBSTATE_READY = 3;
+
+		/**
+		 * Create a new PrintJob.
+		 *
+		 * @param int $tapeSize Tape size to use for job in mm (Default: 12)
+		 * @param int $marginSize Margin size for pages (Default: 14)
+		 * @param int $autoCut Auto cut after how many pages? (Default: 1 - 0 to disable)
+		 * @param bool $halfCut Use half-cutting.
+		 * @param bool $chainPrinting Use chain-printing.
+		 */
+		public function __construct(int $tapeSize = 12, int $marginSize = 14, int $autoCut = 1, bool $halfCut = true, bool $chainPrinting = false) {
+			$this->autoCut = $autoCut;
+			$this->halfCut = $halfCut;
+			$this->chainPrinting = $chainPrinting;
+			$this->tapeSize = $tapeSize;
+			$this->marginSize = $marginSize;
+		}
+
+
+		/**
+		 * Start a new print job.
+		 *
+		 * @return $this This for chaining.
+		 */
+		public function startJob(): PrintJob {
+			$this->job = new PTouchJobBinary();
+			$this->job->printInfo($this->tapeSize);
+			$this->job->mode(($this->autoCut > 0));
+			if ($this->autoCut > 0) {
+				$this->job->cutEachX($this->autoCut);
+			}
+			$this->job->advancedMode($this->halfCut, $this->chainPrinting);
+			$this->job->margin($this->marginSize);
+			$this->job->compressionMode();
+			$this->jobState = PrintJob::JOBSTATE_START;
+
+			return $this;
+		}
+
+		/**
+		 * Add images to our print job.
+		 *
+		 * @param array $images Array of RasterImages to print.
+		 * @return $this This for chaining.
+		 * @throws Exception If there is an error adding the image.
+		 */
+		public function addImages(Array $images): PrintJob {
+			foreach ($images as $image) {
+				$this->addImage($image);
+			}
+
+			return $this;
+		}
+
+		/**
+		 * Add a single image to our print job.
+		 *
+		 * @param RasterImage $image Image to add to job.
+		 * @return $this This for chaining.
+		 * @throws Exception If there is an error adding the image.
+		 */
+		public function addImage(RasterImage $image): PrintJob {
+			if ($this->jobState == PrintJob::JOBSTATE_READY) {
+				throw new Exception('You can not add any more images to a ready job.');
+			}
+			if ($this->jobState != PrintJob::JOBSTATE_START) {
+				throw new Exception('You must call startJob() before adding images.');
+			}
+
+			if ($this->hasImages) { $this->job->printPage(); }
+			$this->job->rasterImage($image);
+			$this->hasImages = true;
+
+			return $this;
+		}
+
+		/**
+		 * Start a new print job.
+		 *
+		 * @return $this This for chaining.
+		 * @throws Exception
+		 */
+		public function endJob(): PrintJob {
+			if (!$this->hasImages) {
+				throw new Exception('You must add at least 1 image.');
+			}
+
+			$this->job->printAndFeed();
+
+			$this->jobState = PrintJob::JOBSTATE_READY;
+			return $this;
+		}
+
+		/**
+		 * Print this job to a remote printer
+		 *
+		 * @param String $ip IP to print to
+		 * @param int $port Port to print to (Default: 9100)
+		 * @throws Exception If the job is not printable or there was an error with the printer
+		 */
+		public function printToIP(String $ip, $port = 9100) {
+			if ($this->jobState != PrintJob::JOBSTATE_READY) {
+				throw new Exception('You can not print an incomplete job.');
+			}
+
+			$fp = @fsockopen($ip, $port, $errno, $errstr, 30);
+			if (!$fp) {
+				throw new Exception('There was an error connecting to the printer: ' . $errstr . '(' . $errno . ')');
+			} else {
+				fwrite($fp, $this->job->getData());
+				fclose($fp);
+			}
+		}
+
+		/**
+		 * Print this job to a local CUPS printer
+		 *
+		 * @param String $printerName Printer name to print to.
+		 * @throws Exception If the job is not printable or there was an error with the printer
+		 */
+		public function printToCUPS(String $printerName) {
+			if ($this->jobState != PrintJob::JOBSTATE_READY) {
+				throw new Exception('You can not print an incomplete job.');
+			}
+
+			if (!file_exists('/usr/bin/lpr')) { throw new Exception('This requires /usr/bin/lpr to exist.'); }
+
+			$descriptorspec = [0 => ['pipe', 'r'], 1 => ['pipe', 'w'], 2 => ['pipe', 'w']];
+
+			$cmd = 'exec /usr/bin/lpr -P '. escapeshellarg($printerName) . ' 2>&1';
+			$pipes = [];
+			$proc = proc_open($cmd, $descriptorspec, $pipes);
+			if ($proc) {
+				fwrite($pipes[0], $this->job->getData());
+				fclose($pipes[0]);
+				fclose($pipes[1]);
+				fclose($pipes[2]);
+			} else {
+				throw new Exception('Error opening LPR');
+			}
+		}
+
+		/**
+		 * Print this print job to STDOUT
+		 *
+		 * @throws Exception If the job is not printable
+		 */
+		public function printToSTDOUT() {
+			if ($this->jobState != PrintJob::JOBSTATE_READY) {
+				throw new Exception('You can not print an incomplete job.');
+			}
+
+			echo $this->job->getData();
+		}
+
+		/**
+		 * Print this print job to STDOUT as HEX for debugging.
+		 *
+		 * @throws Exception If the job is not printable
+		 */
+		public function printHEXToSTDOUT() {
+			if ($this->jobState != PrintJob::JOBSTATE_READY) {
+				throw new Exception('You can not print an incomplete job.');
+			}
+
+			$this->job->displayHex();
+		}
+	}
---
+++ src/RasterImage.php
@@ -0,0 +1,143 @@
+<?php
+
+	namespace ShaneMcC\PTouchPrint;
+
+	/**
+	 * Convert a PNG Image into a brother ptouch-compatible binary.
+	 *
+	 * The image should be horizontal as you want it to come out of the
+	 * printer, no more than 128 pixels tall (Smaller for shorter tape) and as
+	 * long as needed.
+	 */
+	class RasterImage {
+		private array $lines;
+		private bool $mirrored;
+
+		/**
+		 * Create a new RasterImage from an image file.
+		 *
+		 * @param String $imageFile Path to image file
+		 * @param int $maxPixels Max pixels for image height. Leave at default mostly. (Default: 128)
+		 * @param bool $storeMirrored Store the image flipped. Leave at default mostly. (Default: true)
+		 */
+		public function __construct(String $imageFile, $maxPixels = 128, $storeMirrored = true) {
+			// Storage mode.
+			$this->mirrored = $storeMirrored;
+
+			// The image lines.
+			$this->lines = [];
+
+			// Load the image.
+			$original = imagecreatefrompng($imageFile);
+
+			// Convert to greyscale
+			imagefilter($original, IMG_FILTER_GRAYSCALE);
+			imagefilter($original, IMG_FILTER_CONTRAST, -100);
+
+			// Rotate into the orientation we need it for printing.
+			$image = imagerotate($original, 270, 0);
+			imagedestroy($original);
+
+			$width = imagesx($image);
+			$height = imagesy($image);
+
+			// Ensure we print in the center of the label.
+			$offset = ($maxPixels / 2) - ($width / 2);
+			$lineSize = $maxPixels / 8;
+
+			// Top to bottom
+			for ($y = 0; $y < $height; $y++) {
+				// New Line
+				$line = array_fill(0, $lineSize, 0);
+
+				// Left to right
+				for ($x = 0; $x < $width; $x++) {
+					// Check what colour this pixel is.
+					$rgb = imagecolorat($image, $x, $y);
+					$r = ($rgb >> 16) & 0xFF;
+					$g = ($rgb >> 8) & 0xFF;
+					$b = $rgb & 0xFF;
+					// We only support on or off, so if any of the pixels RGB
+					// values are more than "half" on, then we'll treat this as
+					// a pixel to print
+					$pixelOn = ($r > 128 || $g > 128 || $b > 128) ? false : true;
+
+					if ($pixelOn) {
+						// Where in our output do we actually want to put this
+						// pixel.
+						$pixelPos = $x + $offset;
+
+						if ($this->mirrored) {
+							// The image is stored reversed for actually sending to
+							// the printer as it reads right to left.
+							$line[($lineSize - 1) - floor($pixelPos / 8)] += (1 << ($pixelPos % 8));
+						} else {
+							// Non-reversed
+							$line[floor($pixelPos / 8)] += (1 << (7 - ($pixelPos % 8)));
+						}
+					}
+				}
+
+				// Add this line to our lines.
+				$this->lines[] = $line;
+			}
+
+			imagedestroy($image);
+		}
+
+		/**
+		 * Get the lines we need to send.
+		 *
+		 * @return array Array of lines representing this image
+		 */
+		public function getLines(): array {
+			return $this->lines;
+		}
+
+		/**
+		 * How wide is this image?
+		 *
+		 * @return int Width of this image.
+		 */
+		public function getWidth(): int {
+			return count($this->lines[0]) * 8;
+		}
+
+		/**
+		 * How long is this image?
+		 *
+		 * @return int Length of this image.
+		 */
+		public function getLength(): int {
+			return count($this->lines);
+		}
+
+		/**
+		 * Is this image stored mirrored?
+		 *
+		 * @return bool Are we mirrored internally?
+		 */
+		public function isMirrored(): bool {
+			return $this->mirrored;
+		}
+
+		/**
+		 * Utility function for displaying the image to the CLI.
+		 */
+		public function displayImage() {
+			echo '┌', str_repeat('─', $this->getWidth()), '┐', "\n";
+			foreach ($this->lines as $line) {
+				echo '│';
+				foreach (($this->mirrored ? array_reverse($line) : $line) as $bit) {
+					$bin = decbin($bit);
+					if ($this->mirrored) { $bin = strrev($bin); }
+					$bin = str_pad($bin, 8, '0', $this->mirrored ? STR_PAD_RIGHT : STR_PAD_LEFT);
+
+					echo str_replace('1', '█', str_replace('0', ' ', $bin));
+				}
+				echo '│';
+				echo "\n";
+			}
+			echo '└', str_repeat('─', $this->getWidth()), '┘', "\n";
+		}
+	}
